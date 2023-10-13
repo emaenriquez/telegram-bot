393132df5685a5fbaf19c67aafb421b495d72d55
emaenriquez <emaenriquez1985@gmail.com>
2023-10-13 20:36:59 -0300

    nuevas modificaciones

--- index.py
+++ index.py
@@ -1,23 +1,19 @@
-
-
 from telegram import Update
-from telegram.ext import Applicacion, ComandoManejador, ManejadorMensaje, Filtros, TiposContexto
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 
 token = '6480837188:AAGuOMa7Xq8r2Y_Ibk5aOoXqf7dIpmoL-kY'
 nombre_usuario = 'jefemaestrobot'
 
-async def iniciar(update: Update, contexto: TiposContexto):
-    await update.message.reply_text('Hola, soy un bot. ¿En qué puedo ayudarte?')
+def iniciar(update: Update, context: CallbackContext):
+    update.message.reply_text('Hola, soy un bot. ¿En qué puedo ayudarte?')
 
-async def ayuda(update: Update, contexto: TiposContexto):
-    await update.message.reply_text('Ayuda: Puedes decir "Hola" o "Adiós".')
+def ayuda(update: Update, context: CallbackContext):
+    update.message.reply_text('Ayuda: Puedes decir "Hola" o "Adiós".')
 
+def personalizado(update: Update, context: CallbackContext):
+    update.message.reply_text(update.message.text)
 
-async def personalizado(update: Update, contexto: TiposContexto):
-    await update.message.reply_text(update.message.text)
-
-
-def manejar_respuesta(texto: str, contexto: TiposContexto, update: Update):
+def manejar_respuesta(texto: str):
     texto_procesado = texto.lower()
 
     if 'hola' in texto_procesado:
@@ -27,38 +23,57 @@ def manejar_respuesta(texto: str, contexto: TiposContexto, update: Update):
     else:
         return 'No te entiendo'
 
-
-async def manejar_mensaje(update: Update, contexto: TiposContexto):
+def manejar_mensaje(update: Update, context: CallbackContext):
     tipo_mensaje = update.message.chat.type
     texto = update.message.text
 
     if tipo_mensaje == 'group':
         if texto.startswith(nombre_usuario):
             nuevo_texto = texto.replace(nombre_usuario, "")
-            respuesta = manejar_respuesta(nuevo_texto, contexto, update)
+            respuesta = manejar_respuesta(nuevo_texto)
         else:
             return
     else:
-        respuesta = manejar_respuesta(texto, contexto, update)
+        respuesta = manejar_respuesta(texto)
 
-    await update.message.reply_text(respuesta)
+    update.message.reply_text(respuesta)
 
-
-async def error(update: Update, contexto: TiposContexto):
-    print(contexto.error)
-    await update.message.reply_text('Ha ocurrido un error')
-
+def error(update: Update, context: CallbackContext):
+    print(context.error)
+    update.message.reply_text('Ha ocurrido un error')
 
 if __name__ == '__main__':
     print('Iniciando bot')
-    app = Applicacion.builder().token(token).build()
+    updater = Update(token=token, use_context=True)
+    dispatcher = updater.dispatcher
 
-    app.agregar_manejador(ComandoManejador('start', iniciar))
-    app.agregar_manejador(ComandoManejador('help', ayuda))
-    app.agregar_manejador(ComandoManejador('custom', personalizado))
-
-    app.agregar_manejador(ManejadorMensaje(Filtros.texto & ~Filtros.comando, manejar_mensaje))
-    app.agregar_manejador_error(error)
+    dispatcher.add_handler(CommandHandler('start', iniciar))
+    dispatcher.add_handler(CommandHandler('help', ayuda))
+    dispatcher.add_handler(CommandHandler('custom', personalizado))
+    dispatcher.add_handler(MessageHandler(filters.text & ~filters.command, manejar_mensaje))
+    dispatcher.add_error_handler(error)
 
     print('Bot iniciado')
-    app.ejecutar_encuesta(intervalo_encuesta=1, tiempo_limite=10)
+    updater.start_polling()
+    updater.idle()
+
+
+
+# async def error(update: Update, contexto: CallbackContext):
+#     print(contexto.error)
+#     await update.message.reply_text('Ha ocurrido un error')
+
+
+# if __name__ == '__main__':
+#     print('Iniciando bot')
+#     app = Application.builder().token(token).build()
+
+#     app.agregar_manejador(CommandHandler('start', iniciar))
+#     app.agregar_manejador(CommandHandler('help', ayuda))
+#     app.agregar_manejador(CommandHandler('custom', personalizado))
+
+#     app.agregar_manejador(CommandHandler(filtros.texto & ~FileExistsErroriltros.comando, manejar_mensaje))
+#     app.agregar_manejador_error(error)
+
+#     print('Bot iniciado')
+#     app.ejecutar_encuesta(intervalo_encuesta=1, tiempo_limite=10)
